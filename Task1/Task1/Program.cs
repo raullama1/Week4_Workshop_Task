83657257b4dfdf57ae5fcde61f4b8ccb3bf908c4
Raul Lama <tenta@Rauls-MacBook-Air.local>
2025-11-18 00:00:42 +0545

    Task 1 complete.

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First object
+            Student s1 = new Student();
+            s1.name = "Raul";
+            s1.age = 20;
+            s1.section = "C7";
+            
+            // Second object
+            Student s2 = new Student();
+            s2.name = "Tenta";
+            s2.age = 21;
+            s2.section = "C1";
+            
+            // Print first object
+            Console.WriteLine("Student 1: ");
+            Console.WriteLine($"Name: {s1.name}, Age: {s1.age}, Section: {s1.section}");
+            
+            // Print second object
+            Console.WriteLine("Student 2: ");
+            Console.WriteLine($"Name: {s2.name}, Age: {s2.age}, Section: {s2.section}");
+            
+            // Print static
+            Console.WriteLine($"Major: {Student.major}");
+        }
+    }
+}
---
+++ Task1/Task1/Student.cs
@@ -0,0 +1,12 @@
+namespace Task1;
+
+public class Student
+{
+    // Three instance fields
+    public string name;
+    public int age;
+    public string section;
+    
+    // shared by all objects
+    public static string major = "Computer Science";
+}
