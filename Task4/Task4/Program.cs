b7a4f3c8a5d262c3e3b9bf20ee86b11dfa865f22
Raul Lama <tenta@Rauls-MacBook-Air.local>
2025-11-18 09:54:48 +0545

    Task 4 complete.

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player();  // calls default constructor
+            Player p2 = new Player("Raul", 10, 100);  // calls parameterized constructor
+
+            // display p2 details
+            Console.WriteLine(p2.playerName + ", " + p2.level + ", " + p2.health);
+        }
+    }
+}
