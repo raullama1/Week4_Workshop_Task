549e35cc3e0724e844eac9d36ff146f8026e059b
Raul Lama <tenta@Rauls-MacBook-Air.local>
2025-11-18 11:16:03 +0545

    Task 6 complete.

---
+++ Task6/Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter marks: ");
+            bool m = int.TryParse(Console.ReadLine(), out int marks);
+
+            Console.Write("Enter total: ");
+            bool t = int.TryParse(Console.ReadLine(), out int total);
+
+            // BREAKPOINT 1 — before calculation
+
+            double percentage = (double)marks / total * 100;
+
+            // BREAKPOINT 2 — after calculation
+
+            Console.WriteLine("Percentage: " + percentage + "%");
+        }
+    }
+}
